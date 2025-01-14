d3d9a9ac818eb11f76877ee158c82b2026593ec8
Yinuo <s10269277@connect.np.edu.sg>
2025-01-14 14:57:34 +0800

    test

---
+++ FlightAppTest/FlightAppTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FlightAppTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

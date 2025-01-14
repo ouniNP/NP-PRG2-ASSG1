b6955fda8de498b95c39f7172aeae33670f06cfc
Yinuo <s10269277@connect.np.edu.sg>
2025-01-14 14:44:04 +0800

    Fixed using directive

--- FlightApp/Program.cs
+++ FlightApp/Program.cs
@@ -1,4 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Shape;
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("i was here");
 Console.WriteLine("Hello!");

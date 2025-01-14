4ac2ab306b7e3d3cb7d1192ea885108087aae433
Yinuo <s10269277@connect.np.edu.sg>
2025-01-14 14:47:08 +0800

    restart

--- FlightApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shape;
-
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("i was here");
-Console.WriteLine("Hello!");
-
-Shape shape1 = new Shape("blue",20);

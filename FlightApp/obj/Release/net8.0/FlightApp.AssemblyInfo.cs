057cd172f39a3d6d3e4be17ab5f99d6d8e99925d
Yinuo <s10269277@connect.np.edu.sg>
2025-01-14 19:39:52 +0800

    Update FlightApp

--- FlightApp/obj/Release/net8.0/FlightApp.AssemblyInfo.cs
+++ FlightApp/obj/Release/net8.0/FlightApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2697131da9bd19ca4ff1e534f9acac5c9ee89300")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightApp")]

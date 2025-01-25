ea234be7ca5fb361dc0796c0b3ac4a28d14d63ef
Yinuo <s10269277@connect.np.edu.sg>
2025-01-25 17:22:48 +0800

    Full implementation of LWTTFlight.cs

--- FlightApp/LWTTFlight.cs
+++ FlightApp/LWTTFlight.cs
@@ -8,21 +8,36 @@ namespace FlightApp
 {
     class LWTTFlight : Flight
     {
-        public double RequestFee { get; set; }
+        public double RequestFee { get; } = 500;
 
         public LWTTFlight() { }
 
-        public LWTTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, double requestFee) : base(flightNumber, origin, destination, expectedTime)
+        public LWTTFlight(string flightNumber, string origin, string destination, DateTime expectedTime) : base(flightNumber, origin, destination, expectedTime)
         {
             FlightNumber = flightNumber;
             Origin = origin;
             Destination = destination;
             ExpectedTime = expectedTime;
-            RequestFee = requestFee;
+            RequestFee = this.RequestFee;
         }
         public override double CalculateFees()
         {
-            throw new NotImplementedException();
+            const double BOARDING_GATE_BASE_FEE = 300;
+            double totalFees = 0;
+            if (this.Destination.ToUpper().Contains("SIN"))
+            {
+                totalFees = BOARDING_GATE_BASE_FEE + 500 + RequestFee;
+                return totalFees;
+            }
+            else if (this.Origin.ToUpper().Contains("SIN"))
+            {
+                totalFees = BOARDING_GATE_BASE_FEE + 800 + RequestFee;
+                return totalFees;
+            }
+            else
+            {
+                throw new Exception("Invalid operation: Your flight does NOT arrive to SIN/depart from SIN.");
+            }
         }
         public override string ToString()
         {

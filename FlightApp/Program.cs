0f60e95934a201ab2aeea40a1e0b592e8bd2529f
Yinuo <s10269277@connect.np.edu.sg>
2025-01-27 11:28:58 +0800

    Fix Main Loop

--- FlightApp/Program.cs
+++ FlightApp/Program.cs
@@ -253,7 +253,7 @@ while (true)
     }
     if (option == 1)
     {
-        DisplayFlights(FlightsDict,AirlinesDict);
+        DisplayFlights(FlightsDict, AirlinesDict);
         WhiteSpace();
     }
     else if (option == 2)
@@ -280,6 +280,11 @@ while (true)
     else if (option == 7)
     {
         throw new NotImplementedException();
+    }
+    else if (option == 0)
+    {
+        Console.WriteLine("Goodbye!");
+        break;
     }
     else
     {
